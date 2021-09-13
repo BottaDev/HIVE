a787e0344f43517863674fdd2f859d69604c162a
Juli520 <50522986+Juli520@users.noreply.github.com>
2021-09-13 12:47:30 -0300

    Movement, Camera and Shoot

--- Assets/Scripts/TP/ShootTP.cs
+++ Assets/Scripts/TP/ShootTP.cs
@@ -21,23 +21,26 @@ public class ShootTP : MonoBehaviour
         if (Input.GetButton("Fire1") && Time.time >= _nextShoot)
         {
             _nextShoot = Time.time + 1 / fireRate;
-            Shooter();   
+            Shoot();   
         }
     }
 
-    private void Shooter()
+    private void Shoot()
     {
-        shootEffect.Play();
-
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
-        if (Physics.Raycast(playerTP.transform.position, playerTP.transform.forward, out hit, range))
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
         {
+            targetPoint = hit.point;
             TargetTP target = hit.transform.GetComponent<TargetTP>();
-            if(target != null)
+            if (target != null)
                 target.TakeDamage(damage);
 
-            GameObject particle= (GameObject)Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            GameObject particle = (GameObject)Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(particle, 2);
         }
+        else
+            targetPoint = ray.GetPoint(1000);
     }
 }
