2bcece74ae78fd91d2863006a1ee99291d748456
Falkner1993 <70779356+Falkner1993@users.noreply.github.com>
2021-09-26 23:51:44 -0300

    Animator begin
    it is not ready yet. problems with the masl

---
+++ Assets/Animations/AnimationsPlayer/myAnimScripts.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myAnimScripts : MonoBehaviour
+{
+    Animator anim;
+    bool isShooting;
+    bool isRunning;
+    bool isJumping;
+    bool IsDashing;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            anim.SetBool("isJumping",true);
+            Debug.Log("pasa a jump");
+        }
+        else
+        {
+            anim.SetBool("isJumping", false);
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            anim.SetBool("isDashing", true);
+            Debug.Log("pasa a dash");
+        }
+        else
+        {
+            anim.SetBool("isDashing", false);
+        }
+
+
+
+        if(Input.GetKey(KeyCode.Space))
+        {
+            anim.SetBool("isShooting", true);
+            Debug.Log("deberia disparar");
+        }
+        else
+        {
+            anim.SetBool("isShooting", false);
+        }
+    }
+}
