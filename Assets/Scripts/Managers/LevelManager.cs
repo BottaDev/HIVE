d86524d1613a4f2284a5bfc277ff823fae689331
Federico Botta <fedebotta@gmail.com>
2021-10-09 15:14:24 -0300

    Door objective

---
+++ Assets/Scripts/Managers/LevelManager.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class LevelManager : MonoBehaviour
+{
+    [Header("Objective")] 
+    [Tooltip("The minimum number of enemies to kill to open the exit")] public int minEnemies = 10;
+    
+    private GameObject _exitDoor;
+    private int _enemiesKilled = 0;
+    private levelGen _levelGen;
+
+    private void Awake()
+    {
+        _levelGen = GetComponent<levelGen>();
+        
+        EventManager.Instance.Subscribe("OnEnemyDeath", OnEnemyDeath);
+    }
+
+    private void Start()
+    {
+        // Generate the level
+        _levelGen.StartGeneration();
+        _exitDoor = GameObject.FindWithTag("Door");
+        
+        // Bake map navmesh
+        FindObjectOfType<NavMeshSurface>().BuildNavMesh();
+    }
+    
+    private void OnEnemyDeath(params object[] parameters)
+    {
+        _enemiesKilled++;
+
+        // Open the exit door
+        if (_enemiesKilled >= minEnemies && _exitDoor.activeSelf)
+            _exitDoor.SetActive(false);
+    }
+}
