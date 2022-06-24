cecbded5ad82d23b9841d2243ec3ce7641fc0e6d
Shauzen-Prog <80788112+Shauzen-Prog@users.noreply.github.com>
2022-06-24 15:04:01 -0300

    Emilrang Bullet Hell Attack

---
+++ Assets/Scripts/Boss/Emilrang/BulletHellAttack.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletHellAttack : MonoBehaviour
+{
+    public int numberOfProjectiles;
+    public float projectileSpeed;
+
+    public GameObject projectileBulletHell;
+
+    public GameObject attackPoint;
+
+    private Vector3 startPoint;
+    private const float radius = 1f;
+
+    // Start is called before the first frame update
+
+    public void SpawnProjectile(int _numberOfProjectiles)
+    {
+        startPoint = attackPoint.transform.position;
+
+        float angleStep = 360f / _numberOfProjectiles;
+        float angle = 0f;
+
+        for (int i = 0; i < _numberOfProjectiles - 1; i++)
+        {
+            //Direction Calculation
+            float projectileDirXPosition = startPoint.x + Mathf.Sin((angle * Mathf.PI) / 180) * radius;
+            float projectileDirYPosition = startPoint.y + Mathf.Cos((angle * Mathf.PI) / 180) * radius;
+
+            Vector3 projectileVector = new Vector3(projectileDirXPosition, projectileDirYPosition, 0);
+            Vector3 projectileMoveDirection = (projectileVector - startPoint).normalized * projectileSpeed;
+
+            GameObject tempObj = Instantiate(projectileBulletHell, startPoint, Quaternion.identity);
+            tempObj.GetComponent<Rigidbody>().velocity = new Vector3(projectileMoveDirection.x, 0, projectileMoveDirection.y);
+
+            angle += angleStep;
+        }
+    }
+
+}
